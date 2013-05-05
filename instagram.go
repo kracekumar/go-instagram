3da1b38fcbba9c633107818cc9a746758dbd87ba
kracekumar <me@kracekumar.com>
2013-05-05 22:18:14 +0530

    started in goauth2

--- instagram.go
+++ instagram.go
@@ -2,10 +2,12 @@ package instagram
 
 import (
 	//	"errors"
+	"code.google.com/p/goauth2/oauth"
 	//	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
+
+//	"net/url"
 )
 
 //API docs: http://instagram.com/developer
@@ -23,12 +25,11 @@ type Jar struct {
 }
 
 type Instagram struct {
-	ClientSecret string
-	ClientId     string
-	AccessToken  string
-	RedirectURI  string
-	client       *http.Client
-	jar          *Jar
+	AccessToken string
+	client      *http.Client
+	jar         *Jar
+	Code        string
+	OauthConfig *oauth.Config
 }
 
 func check_error(err error) {
@@ -38,20 +39,27 @@ func check_error(err error) {
 	}
 }
 
-func (i Instagram) resolve_redirect(resp *http.Response) *http.Response {
+/*func (i Instagram) resolve_redirect(resp *http.Response) *http.Response {
 	for count := 0; count < REDIRECT_LIMIT; count++ {
+		log.Println(resp.StatusCode)
 		if resp.StatusCode == 200 || resp.StatusCode >= 400 {
 			return resp
 		} else {
-			for code := range REDIRECT_CODES {
-				log.Print("inside loop")
+			for _, code := range REDIRECT_CODES {
 				if code == resp.StatusCode {
 					location, err := resp.Location()
 					check_error(err)
-					method := resp.Request.Method
+					//method := resp.Request.Method
+					//				log.Println(resp.Header)
+					//rbody, err := ioutil.ReadAll(resp.Body)
+					//log.Println(resp.Request.Form)
+					//check_error(err)
+					//log.Println(string(rbody))
+					//log.Println(location)
+					resp, err := i.client.PostForm(location.String(), i.URLValues)
 					log.Println(resp)
-					log.Println(method)
-					log.Println(location)
+					log.Println(resp.StatusCode)
+					break
 				}
 			}
 
@@ -59,42 +67,36 @@ func (i Instagram) resolve_redirect(resp *http.Response) *http.Response {
 	}
 	panic("MAximim Redirects")
 	return resp
-}
+}*/
 
 func NewInstagram(client_id, client_secret string) *Instagram {
 	i := &Instagram{}
-	i.ClientId = client_id
-	i.ClientSecret = client_secret
+	i.OauthConfig = &oauth.Config{
+		ClientId:     client_id,
+		ClientSecret: client_secret,
+	}
 	i.client = &http.Client{}
 	return i
 }
 
-func (i Instagram) Authenticate(redirect_uri, scope string) {
-	i.RedirectURI = redirect_uri
+func (i Instagram) Authenticate(redirect_url, scope string) string {
 	//first step: Direct user to Instagram authorization URL
-	instagram_url, err := url.Parse(AUTHORIZATION_URL)
-	check_error(err)
-	var resp *http.Response
-	if scope != "" {
-		resp, err = i.client.PostForm(instagram_url.String(), url.Values{"client_id": {i.ClientId}, "redirect_uri": {i.RedirectURI},
-			"response_type": {"code"}, "scope": {scope}})
-	} else {
-		resp, err = i.client.PostForm(instagram_url.String(), url.Values{"client_id": {i.ClientId}, "redirect_uri": {i.RedirectURI},
-			"response_type": {"code"}})
-	}
-	check_error(err)
-	log.Println(resp)
-	i.resolve_redirect(resp)
+	i.OauthConfig.AuthURL = AUTHORIZATION_URL
+	i.OauthConfig.TokenURL = ACCESS_URL
+	i.OauthConfig.RedirectURL = redirect_url
+	return i.OauthConfig.AuthCodeURL("")
+	//i.OauthConfig.AuthCodeURL("")
 }
 
-func (i Instagram) GetAccessToken(code string) *http.Response {
+func (i Instagram) GetAccessToken(code string) {
 	//Third step of oauth2.0: Do a Post
-	u, err := url.Parse(ACCESS_URL)
-	check_error(err)
+	t := &oauth.Transport{oauth.Config: i.OauthConfig}
+	t.Exchange(code)
+	log.Println(t)
+	/*check_error(err)
 	resp, err := i.client.PostForm(u.String(), url.Values{"client_id": {i.ClientId}, "client_secret": {i.ClientSecret},
 		"redirect_uri": {i.RedirectURI}, "code": {code}, "grant_type": {"authorization_code"}})
 	check_error(err)
 	log.Println("Get Access Token")
-	log.Println(resp)
-	return resp
+	log.Println(resp)*/
 }
